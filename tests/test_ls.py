eadb5871d2edb8393399afcecb52e5b686822985
mwip <mwip@users.noreply.github.com>
2026-01-02 23:12:11 +0100

    lint: ignore some rules in tests, fix test_ls.py

--- tests/test_ls.py
+++ tests/test_ls.py
@@ -1,9 +1,7 @@
-import time
-
 from explorzarr.ls import ls_fn
 
 
-def test_with_fixture(fixture_zarr_small, capsys):
+def test_ls_correct_ouput(fixture_zarr_small, capsys):
     expected_output = """Name        : 
 Type        : Group
 Zarr format : 3
@@ -18,7 +16,7 @@ No. groups  : 1
     └── ar1 (123, 42, 42) float64
  
 
-"""
+"""  # noqa: W291, W293
     ls_fn(store=fixture_zarr_small, path=None, level=None)
 
     captured = capsys.readouterr()
